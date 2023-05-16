304798461a46c41b936ec80f073917181ec5bea5
kirillka922 <gmkir92@gmail.com>
2023-05-16 15:00:45 +0800

    2 вариант программы

--- src/main.js
+++ src/main.js
@@ -1,21 +1,20 @@
-function createBa() {}
-
 inpBarChart.addEventListener("blur", function (ev) {
   contBarChart.innerHTML = "";
-  let sortVals = {};
+  let sortVals = [];
   let massNumb = ev.target.value.split(" ").filter(function (val) {
-    if (val !== " " && !isNaN(Number(val) % 2)) return val;
+    if (val !== " " && !isNaN(Number(val) % 2)) return Number(val);
   });
   let lenBarChart = Math.round(100 / massNumb.length);
-  for (let i = 0; i < massNumb.length; i++) {
-    sortVals[massNumb[i]] = i + 1;
-  }
-  for (let i = 0; i < massNumb.length; i++) {
-    let powerOfWal = Object.keys(sortVals).indexOf(massNumb[i]) + 1;
+
+  sortVals = massNumb.sort((a, b) => {
+    return a - b;
+  });
+
+  for (let i = 0; i < sortVals.length; i++) {
     let oneBarElement = document.createElement("div");
     oneBarElement.classList.add("element");
-    oneBarElement.style.height = `${powerOfWal * lenBarChart}px`;
-    oneBarElement.innerHTML = `<p class="textContent">${massNumb[i]}</p>`;
+    oneBarElement.style.height = `${(i + 1) * lenBarChart}px`;
+    oneBarElement.innerHTML = `<p class="textContent">${sortVals[i]}</p>`;
     contBarChart.appendChild(oneBarElement);
   }
 });
