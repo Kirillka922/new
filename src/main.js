7198a754ea27f985e534290599db2e281a9ee570
kirillka922 <gmkir92@gmail.com>
2023-05-21 21:33:39 +0800

    the program without sort, correct styles, procent

--- src/main.js
+++ src/main.js
@@ -1,43 +1,50 @@
-const buttonCreate = document.querySelector(".createChart");
-const buttonSort = document.querySelector(".sortChart");
+const buttonCreate = document.getElementById("createChart");
+const buttonSort = document.getElementById("sortChart");
 
-buttonCreate.addEventListener("click", () => buildChart("buildChart"));
-buttonSort.addEventListener("click", () => buildChart("sortVals"));
-
-function buildChart(action) {
+function createChart(mass = undefined) {
   const input = document.querySelector(".сhartInp");
-  const container = document.querySelector(".colums-container");
-  container.querySelectorAll("div").forEach((colum) => colum.remove());
+  const container = document.querySelector(".container");
 
-  const massNumb = input.value.split(" ").filter(function (val) {
+  container.querySelectorAll(".colum").forEach((colum) => colum.remove());
+  const massNumb = mass ? mass : filterMass(input.value);
+
+  for (let i = 0; i < massNumb.length; i++) {
+    const newColum = document.createElement("div");
+    newColum.classList.add("colum");
+    newColum.style.height = `${
+      (massNumb[i] / Math.max(...massNumb)) * 100 + 20
+    }px`;
+
+    newColum.textContent = massNumb[i];
+    container.appendChild(newColum);
+  }
+}
+
+function sortChart() {
+  const input = document.querySelector(".сhartInp");
+  const result = filterMass(input.value);
+  result.sort((a, b) => {
+    return a - b;
+  });
+  return createChart(result);
+}
+
+function filterMass(mass) {
+  const massNumb = mass.split(" ").filter(function (val) {
     if (val !== " " && isFinite(Number(val))) {
       return val;
     }
   });
-
-  if (action == "sortVals") {
-    massNumb.sort((a, b) => {
-      return a - b;
-    });
-  }
-  const clone = [...massNumb];
-  const sortMass = clone.sort((a, b) => {
-    return a - b;
-  });
-  const columSize = 100 / massNumb.length;
-  const sizeColums = {};
-  sortMass.forEach((element, i) => {
-    sizeColums[element] = (i + 1) * columSize;
-  });
-
-  for (let i = 0; i < massNumb.length; i++) {
-    let newColum = document.createElement("div");
-    newColum.classList.add(`box-${i}`);
-    newColum.style.minHeight = `${sizeColums[massNumb[i]]}px`;
-    newColum.style.order = `${i}`;
-    let newSpan = document.createElement("span");
-    newSpan.textContent = massNumb[i];
-    newColum.appendChild(newSpan);
-    container.appendChild(newColum);
-  }
+  return massNumb.map((string) => Number(string));
 }
+
+function unblockBtnSort() {
+  const buttonSort = document.getElementById("sortChart");
+  buttonSort.disabled = false;
+}
+
+buttonSort.addEventListener("click", () => sortChart());
+buttonCreate.addEventListener("click", function () {
+  createChart();
+  unblockBtnSort(); //i thought that i can perform this action at the "createChart" function but our function doesn't need to know about the button!
+});
