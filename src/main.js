cfefc5f85587bb13f57b239489bef0e641fc772d
kirillka922 <gmkir92@gmail.com>
2023-06-01 19:02:46 +0800

    the recursion has been removed

--- src/main.js
+++ src/main.js
@@ -24,17 +24,6 @@ function createChart() {
   if (validArray.length > 1) showBtnSort(true);
 }
 
-function sort(array) {
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 1; j < array.length - i; j++) {
-      if (array[j - 1] > array[j]) {
-        [array[j], array[j - 1]] = [array[j - 1], array[j]];
-      }
-    }
-  }
-  return array;
-}
-
 function sortChartOrder() {
   const container = document.querySelector(".container");
   const columnsArray = container.querySelectorAll(".column");
