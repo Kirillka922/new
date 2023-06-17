7dbdff50a3dbf6c157ee990abe857ed5923465eb
kirillka922 <gmkir92@gmail.com>
2023-06-17 18:09:09 +0800

    was removed  recolor columns function

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+const MINIMUM_HEIGHT = 20;
+const STATIC_INDENT = 6;
 const buttonCreate = document.getElementById("createChart");
 const buttonSortDom = document.getElementById("sortColumnsDom");
 const input = document.querySelector(".сhartInp");
@@ -6,18 +8,16 @@ function printColumns(array) {
   const container = document.querySelector(".container");
 
   container.querySelectorAll(".column").forEach((column) => column.remove());
-  const MINIMUMHAIGHT = 20;
-  const STATICINDENT = 6;
 
   for (let i = 0; i < array.length; i++) {
-    const haightColumn = (array[i] / Math.max(...array)) * 100 + MINIMUMHAIGHT;
+    const heightColumn = (array[i] / Math.max(...array)) * 100 + MINIMUM_HEIGHT;
     const newColumn = document.createElement("div");
     newColumn.classList.add("column");
-    newColumn.style.height = `${haightColumn}px`;
+    newColumn.style.height = `${heightColumn}px`;
     newColumn.textContent = array[i];
     container.appendChild(newColumn);
     const leftForColumn = newColumn.offsetWidth * i;
-    const indentForColumn = (1 + i) * STATICINDENT;
+    const indentForColumn = (1 + i) * STATIC_INDENT;
     newColumn.style.left = `${leftForColumn + indentForColumn}px`;
   }
 }
@@ -46,17 +46,29 @@ function sortChart() {
   let position = 0;
   let cycleNumber = 1; //it is easier to start with a number one because cycle number can't be 0
   //in opposite case we should correct a cycle number later
-  let sortTimerId = setInterval(() => runSorting(), 1000);
-
-  let columnsArray = Array.from(getColumns());
+  const columnsArray = Array.from(getColumns());
 
   function runSorting() {
+    const isNewCycle = position == 0 && cycleNumber > 1;
+    const isNotFirstElem = position > 0;
+    const previousColumn = columnsArray[position - 1];
+    const lastColumnPreviousCycle = columnsArray.length - cycleNumber + 1;
+
+    if (isNotFirstElem) {
+      previousColumn.classList.remove("columnSort"); // we need to remove style after the cycle
+      //because we need hold the color on the element for a 1 second
+    }
+
+    if (isNewCycle) {
+      columnsArray[lastColumnPreviousCycle - 1].classList.remove("columnSort");
+      columnsArray[lastColumnPreviousCycle].classList.remove("columnSort");
+      //after the first cycle we need to remove the color of last elements
+    }
+
     if (!checkSortAttrib()) {
       clearInterval(sortTimerId);
       return;
     }
-
-    removeColorElem();
 
     if (cycleNumber > columnsArray.length - 1) {
       removeSortAttribute();
@@ -70,11 +82,25 @@ function sortChart() {
     const firstNumber = Number(firstColumn.textContent); //we need to get numbers inside of columns for check
     const secondNumber = Number(secondColumn.textContent);
 
-    paintNextElements(firstColumn, secondColumn);
+    firstColumn.classList.add("columnSort");
+    secondColumn.classList.add("columnSort");
 
     if (firstNumber > secondNumber) {
-      columnsArray = replacementColumns(columnsArray, position);
+      [columnsArray[position], columnsArray[position + 1]] = [
+        columnsArray[position + 1],
+        columnsArray[position],
+      ];
+
+      const firstCord =
+        firstColumn.getBoundingClientRect().left + window.pageXOffset; //if we will have a long row of columns we need to exclude scroll
+      const secondCord =
+        secondColumn.getBoundingClientRect().left + window.pageXOffset;
+      [firstColumn.style.left, secondColumn.style.left] = [
+        `${secondCord}px`,
+        `${firstCord}px`,
+      ];
     }
+
     position++;
     const lengthArray = columnsArray.length - 1;
     const lastNumberForSort = lengthArray - cycleNumber;
@@ -84,40 +110,7 @@ function sortChart() {
       cycleNumber++;
     }
   }
-}
-
-function replacementColumns(columnsArray, position) {
-  const firstColumn = columnsArray[position];
-  const secondColumn = columnsArray[position + 1];
-
-  [columnsArray[position], columnsArray[position + 1]] = [
-    columnsArray[position + 1],
-    columnsArray[position],
-  ];
-
-  const firstCord =
-    firstColumn.getBoundingClientRect().left + window.pageXOffset; //if we will have a long row of columns we need to exclude scroll
-  const secondCord =
-    secondColumn.getBoundingClientRect().left + window.pageXOffset;
-  [firstColumn.style.left, secondColumn.style.left] = [
-    `${secondCord}px`,
-    `${firstCord}px`,
-  ];
-
-  return columnsArray;
-}
-
-function paintNextElements(firstColumn, secondColumn) {
-  firstColumn.classList.add("columnSort");
-  secondColumn.classList.add("columnSort");
-}
-
-function removeColorElem() {
-  const allColumns = getColumns();
-  allColumns.forEach((column) => {
-    const isClass = column.classList.contains("columnSort");
-    if (isClass) column.classList.remove("columnSort");
-  });
+  const sortTimerId = setInterval(() => runSorting(), 1000);
 }
 
 function getColumns() {
